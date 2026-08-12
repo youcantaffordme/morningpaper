0884972531387119b34d81aba5bab9f0b0abc582
jay <bleujaejr@gmail.com>
2026-08-11 19:57:36 -0700

    Add plugin metadata

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "morningpaper",
+    fullname = _("Morning Paper"),
+    description = _("Build a clean personal morning newspaper from RSS/Atom feeds directly on KOReader."),
+}
